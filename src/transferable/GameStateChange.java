4007f10584ec601b246203a5a1d7720ce5a1b51a
Jordan Blackadar <jordan.blackadar@outlook.com>
2017-05-09 23:02:55 -0400

    Removed ridiculously unnecessary class

--- src/transferable/GameStateChange.java
+++
@@ -1,9 +0,0 @@
-package transferable;
-
-public class GameStateChange {
-    public boolean isStarted;
-
-    public GameStateChange(boolean isStarted){
-        this.isStarted = isStarted;
-    }
-}
